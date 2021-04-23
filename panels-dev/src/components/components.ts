9092d53ad6b1834907fb885bb68a1e55289d94a5
Riccardo Brero <riki.brero@gmail.com>
2021-04-24 00:09:26 +0200

    Init

---
+++ panels-dev/src/components/components.ts
@@ -0,0 +1,4 @@
+import './base/panel-base';
+
+import './hero/hero-1';
+import './hero/hero-2';
---
+++ panels-dev/src/pages/home-page.ts
@@ -0,0 +1,20 @@
+import { LitElement, html, css } from 'lit';
+import { customElement } from 'lit/decorators.js';
+
+import { utilities } from '../styles/panel-utilities';
+
+@customElement('home-page')
+export class HomePage extends LitElement {
+  static styles = [utilities, css``];
+
+  render() {
+    return html`
+      <div class="page">
+        <div class="page-container">
+          <hero-1></hero-1>
+          <hero-2></hero-2>
+        </div>
+      </div>
+    `;
+  }
+}
