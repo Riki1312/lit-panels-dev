8677c4b5f0e43d2d69117ca57d1ed0ca03b1211d
Riccardo Brero <riki.brero@gmail.com>
2021-05-05 18:14:28 +0200

    Test

--- panels-dev/rollup.config.js
+++ panels-dev/rollup.config.js
@@ -1,8 +1,9 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
 import html from '@web/rollup-plugin-html';
 
 export default {
   input: 'public/*.html',
   output: { dir: 'dist' },
-  plugins: [nodeResolve(), html()]
+  plugins: [nodeResolve(), typescript(), html()]
 };
